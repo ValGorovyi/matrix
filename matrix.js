6fb812bf348b9140d5fe90a5b045d71677a32b8d
ValGorovyi <valvetigu@gmail.com>
2022-01-21 11:49:15 +0200

    1

--- matrix.js
+++ matrix.js
@@ -1,4 +1,13 @@
-function matrixAnimation(){
+function matrixAnimation() {
     let body = document.body
-    body.innerHTML +=" " + Math.round(Math.random())
+    body.style.background = 'rgb(39, 34, 34)'
+    body.innerHTML = ''
+    f = setInterval(() => {
+        body.innerHTML += " " + Math.round(Math.random())
+    }, 00,70);
+    setTimeout(() => {
+        clearInterval(f)
+        body.style.background = '#030373'
+        body.innerHTML = 'error. goodbye world'
+    }, 10000);
 }
